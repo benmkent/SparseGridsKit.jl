a9a52c751059b80e3190836bb81342de14fac42c
Ben Kent <benkent@live.co.uk>
2024-10-24 20:51:53 +0200

    correct filename for test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 @testset "SparseGridsKit.jl tests" begin
 
     @testset "Sparse Grid tests" begin
-        include("sparsegrids_tests.jl")
+        include("sparsegrids_test.jl")
     end
 
 end

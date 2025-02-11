a476966249cc95e1959b458a6aca6dc072d71b06
Ben Kent <benkent@live.co.uk>
2025-02-11 12:46:15 +0100

    Refactor the adaptive algorithm into SOLVE ESTIMATE MARK REFINE functions Fixes #4

--- src/adaptivesparsegrids.jl
+++ src/adaptivesparsegrids.jl
@@ -36,17 +36,11 @@ function adaptive_sparsegrid(f, ndims; maxpts = 100, proftol=1e-4, rule = doubli
         RM = get_reduced_margin(MI)
         MI_enhanced = add_mi(MI, RM)
         sg_enhanced = create_sparsegrid(MI_enhanced; rule=rule, knots=knots)
-        sg_map = mapfromto(sg,sg_enhanced)
-        Z_enhanced = get_grid_points(sg_enhanced)
 
         # SOLVE
-        f_on_z_enhanced = Vector{typeof(f_on_z[1])}(undef,get_n_grid_points(sg_enhanced))
-        # Fill old evaluations
-        f_on_z_enhanced[sg_map] = f_on_z
-        # Fill new evaluations
-        new_points = setdiff(1:get_n_grid_points(sg_enhanced), sg_map)
-        f_on_z_enhanced[new_points] = [f(z) for z in Z_enhanced[new_points]]
+        f_on_z_enhanced = adaptive_solve(sg,sg_enhanced, f_on_z)
 
+        # Update precompute_lagrange_integrals if necessary
         if maximum([maximum(α) for α in get_mi(get_mi_set(sg_enhanced))])  > maxmi
             maxmi += 3
             pcl = precompute_lagrange_integrals(maxmi, knots, rule)
@@ -54,32 +48,18 @@ function adaptive_sparsegrid(f, ndims; maxpts = 100, proftol=1e-4, rule = doubli
 
         # ESTIMATE
         # Interpolate to new grid
-        f_sg_on_z_enhanced = interpolate_on_sparsegrid(sg,f_on_z, Z_enhanced)
-        p_α = Vector{Float64}(undef, length(get_mi(RM)))
-        for (i,α) in enumerate(get_mi(RM))
-            sg_α = create_sparsegrid(add_mi(MI,α), rule=rule, knots=knots)
-            sg_map_α = mapfromto(sg_α,sg_enhanced)
-            f_on_z_α = f_on_z_enhanced[sg_map_α]
-            
-            f_diff_α = f_on_z_α - f_sg_on_z_enhanced[sg_map_α]
+        p_α = adaptive_estimate(sg, sg_enhanced, f_on_z_enhanced, pcl)
 
-            p_α[i] = compute_profit(sg_α, f_diff_α,pcl)
-        end
-        
         # MARK
-        (p_max, i_max) = findmax(identity, p_α)
-        α_max = get_mi(RM)[i_max]
+        α_marked = adaptive_mark(RM, p_α, θ)
 
         # TERMINATE
-        if terminate_loop(sg, p_α, maxpts, proftol)
+        if adaptive_terminate(sg, p_α, maxpts, proftol)
             break
         end
 
         # REFINE
-        MI = add_mi(MI, α_max)
-        sg = create_sparsegrid(MI; rule=rule, knots=knots)
-        sg_map_refine = mapfromto(sg,sg_enhanced)
-        f_on_z = f_on_z_enhanced[sg_map_refine]
+        sg, f_on_z = adaptive_refine(sg, α_marked, rule, knots)
 
         @info "Iteration: "*string(kk)*"    Number of points: "*string(get_n_grid_points(sg))*"    Max profit: "*string(p_max)
     end
@@ -87,6 +67,61 @@ function adaptive_sparsegrid(f, ndims; maxpts = 100, proftol=1e-4, rule = doubli
     @info "Finished in "*string(kk)*" iterations"
 
     return (sg, f_on_z)
+end
+
+"""
+    adaptive_solve(sg,sg_enhanced, f_on_z)
+
+Computes function f on the new grid points in sg_enhanced
+
+# Arguments
+- `sg`: Sparse grid.
+- `sg_enhanced`: Enhanced sparse grid.
+- `f_on_z`: Function evaluations on the sparse grid `sg`.
+
+# Returns
+- Function evaluations on the enhanced sparse grid `sg_enhanced`.
+"""
+function adaptive_solve(sg,sg_enhanced, f_on_z)
+    Z_enhanced = get_grid_points(sg_enhanced)
+    sg_map = mapfromto(sg,sg_enhanced)
+    f_on_z_enhanced = Vector{typeof(f_on_z[1])}(undef,get_n_grid_points(sg_enhanced))
+    # Fill old evaluations
+    f_on_z_enhanced[sg_map] = f_on_z
+    # Fill new evaluations
+    new_points = setdiff(1:get_n_grid_points(sg_enhanced), sg_map)
+    f_on_z_enhanced[new_points] = [f(z) for z in Z_enhanced[new_points]]
+    return f_on_z_enhanced
+end
+
+"""
+    adaptive_estimate(sg, sg_enhanced, f_on_z_enhanced, pcl)
+
+Estimates the profit of adding multi-indices {α} in reduced margin to the sparse grid.
+
+# Arguments
+- `sg`: Sparse grid.
+- `sg_enhanced`: Enhanced sparse grid.
+- `f_on_z_enhanced`: Function evaluations on the enhanced sparse grid.
+- `pcl`: Precomputed Lagrange integrals.
+
+# Returns
+- Vector of profits for each multi-index α.
+"""
+function adaptive_estimate(sg, sg_enhanced, f_on_z_enhanced, pcl)
+    Z_enhanced = get_grid_points(sg_enhanced)
+    f_sg_on_z_enhanced = interpolate_on_sparsegrid(sg,f_on_z, Z_enhanced)
+    p_α = Vector{Float64}(undef, length(get_mi(RM)))
+    for (i,α) in enumerate(get_mi(RM))
+        sg_α = create_sparsegrid(add_mi(MI,α), rule=rule, knots=knots)
+        sg_map_α = mapfromto(sg_α,sg_enhanced)
+        f_on_z_α = f_on_z_enhanced[sg_map_α]
+        
+        f_diff_α = f_on_z_α - f_sg_on_z_enhanced[sg_map_α]
+
+        p_α[i] = compute_profit(sg_α, f_diff_α,pcl)
+    end
+    return p_α
 end
 
 """
@@ -107,7 +142,31 @@ function compute_profit(sg_α, f_diff_α, pcl)
 end
 
 """
-    terminate_loop(sg, p_α, maxpts, proftol)
+    adaptive_mark(α, p_α, θ=1e-4)
+
+Dorfler style marking of multi-indices based on profits
+
+# Arguments
+- `α`: Multi-indices in reduced margin.
+- `p_α`: Vector of profits.
+- `θ`: Threshold for marking. Default is `1e-4`.
+"""
+function adaptive_mark(α, p_α, θ=1e-4)
+    idx_sorted = sortperm(p_α, rev=true) 
+    # Sort indices by decreasing p_α
+    p_sorted = p_α[idx_sorted]
+        
+    p_total = sum(p_sorted)
+    p_cumsum = cumsum(p_sorted)
+    
+    # Dorfler marking
+    k = findfirst(p_cumsum .≥ θ * p_total)
+    α_marked = α[idx_sorted[1:k]]
+    return α_marked
+end
+
+"""
+   adaptive_terminate(sg, p_α, maxpts, proftol)
 
 Test profits and sparse grid to determine loop termination
 
@@ -120,7 +179,7 @@ Test profits and sparse grid to determine loop termination
 # Returns
 - Flag to indicate loop termination if `maxpts` has been reached, or `proftol` attained.
 """
-function terminate_loop(sg, p_α, maxpts, proftol)
+function adaptive_terminate(sg, p_α, maxpts, proftol)
     retval = false
     if get_n_grid_points(sg) >= maxpts
         @info "Reached max points: "*string(maxpts)
@@ -131,3 +190,25 @@ function terminate_loop(sg, p_α, maxpts, proftol)
     end
     return retval
 end
+
+"""
+    adaptive_refine(sg, α_marked, rule, knots)
+
+Refines the sparse grid based on marked multi-indices
+
+# Arguments
+- `sg`: Sparse grid.
+- `α_marked`: Marked multi-indices.
+- `rule`: Level function(s) for sparse grid.
+- `knots`: Knot function(s) for sparse grid.
+
+# Returns
+- `sg`: Refined sparse grid.
+- `f_on_z`: Function evaluations on the refined sparse grid.
+"""
+    MI = get_mi_set(sg)
+    MI = add_mi(MI, MISet(α_max))
+    sg = create_sparsegrid(MI; rule=rule, knots=knots)
+    sg_map_refine = mapfromto(sg,sg_enhanced)
+    f_on_z = f_on_z_enhanced[sg_map_refine]
+return sg, f_on_z

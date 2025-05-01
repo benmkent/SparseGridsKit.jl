b0b624b60c041b2d91e80bccb76ef50898fdf3a6
Ben Kent <benkent@live.co.uk>
2025-05-01 19:25:16 +0200

    test set was truncated!

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,20 @@
 using Test, SparseGridsKit, Documenter, LinearAlgebra
 
 @testset "SparseGridsKit.jl tests" begin
-    # include("knots_test.jl")
-    # include("multiindexsets_test.jl")
-    # include("sparsegrids_test.jl")
-    # include("interpolation_test.jl")
-    # include("adaptive_test.jl")
-    # include("multifidelity_tests.jl")
-    # include("integration_test.jl")
+    include("knots_test.jl")
+    include("multiindexsets_test.jl")
+    include("sparsegrids_test.jl")
+    include("interpolation_test.jl")
+    include("adaptive_test.jl")
+    include("multifidelity_tests.jl")
+    include("integration_test.jl")
     include("spectral_test.jl")
-    # include("sgmk_test.jl")
-    # include("aqua_test.jl")
-    # include("plots_test.jl")
+    include("sgmk_test.jl")
+    include("aqua_test.jl")
+    include("plots_test.jl")
+    include("differentiation_test.jl")
 end
 
-# @testset "doctest" begin
-#     doctest(SparseGridsKit)
-# end
+@testset "doctest" begin
+    doctest(SparseGridsKit)
+end
